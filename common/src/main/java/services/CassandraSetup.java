a679df41249ab2559679ddf9d405f2335913469c
Ryan Knight <ryknight@starbucks.com>
2017-03-30 23:32:12 -0600

    fixing cassandra setup

--- common/src/main/java/services/CassandraSetup.java
+++ common/src/main/java/services/CassandraSetup.java
@@ -1,19 +1,8 @@
 package services;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import play.libs.Json;
-
-import java.util.List;
-
 public class CassandraSetup {
 
     public static void main(String[] args) {
-        //CassandraHelper.getInstance().createPianoKeyspace();
-
-        List<String> allPianoSongs = CassandraHelper.getInstance().getAllPianoSongs();
-        JsonNode pianoSongJson = Json.toJson(allPianoSongs);
-        String s = pianoSongJson.toString();
-        System.out.println("s = " + s);
-
+        CassandraHelper.getInstance().createPianoKeyspace();
     }
 }
